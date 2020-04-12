d324c30e9993d989b746e851d2c7f6534d2a84de
kriskirimi <60326060+kriskirimi@users.noreply.github.com>
2020-04-12 17:55:13 +0300

    update

--- Mombasa and Nairobi County 2020 Census-Access to Water Pie Charts.py
+++ Mombasa and Nairobi County 2020 Census-Access to Water Pie Charts.py
@@ -6,7 +6,6 @@ Created on Thu Mar 19 17:28:52 2020
 """
 import pandas as pd
 import matplotlib.pyplot as plt
-import matplotlib 
 df = pd.read_csv(r'C:\Users\KIRIMI\Documents\GitHub\Kenya-Access-to-Water-and-Sanitation-Census2019-Analysis-and-Visualization\Data\volume_4-table-2.15_chouseholds-by-main-source-of-drinking-water-area-of-residence-county-and-su.csv')
 df.head(5)
 df.shape
@@ -24,20 +23,20 @@ cols = (['Conventional Households', 'Pond(%)', 'Dam/Lake(%)',
 df[cols] = df[cols].apply(pd.to_numeric, errors='coerce')
 df1 = df[df['County/ Sub-County'].str.contains('NAIROBI|MOMBASA')]
 df1.set_index('County/ Sub-County', inplace=True)
-df1.sort_values(by=['NAIROBI CITY', 'MOMBASA'], 
+df1.sort_values(by=['NAIROBI CITY'], 
                 axis=1, ascending=False, inplace=True)
 percent_to_population = lambda x:x/100* df1['Conventional Households']
 df2=df1.iloc[:,1:].apply(percent_to_population)
 df3 = df2.iloc[:,0:7].T
-fig, (ax1, ax2) = plt.subplots(1,2, figsize=(3,6))
-plt.style.use('seaborn')
-explode = (0.05, 0.05, 0.0, 0.0, 0.0, 0.0, 0.0)
-plt.tight_layout()
-ax1.pie(df3['MOMBASA'], labels=df3.index, explode=explode, 
-        autopct='%1.1f%%', radius=0.5)
-ax2.pie(df3['NAIROBI CITY'], labels=df3.index, explode=explode, 
-        autopct='%1.1f%%', radius=1.0)
-matplotlib.rcParams['font.size'] = 12
-ax1.set_title('Mombasa County Access to Water, Census 2019')
-ax2.set_title('Nairobi County Access to Water, Census 2019')
+
+df3.columns=['Mombasa County Access to Water, Census 2019', 
+             'Nairobi City Access to Water, Census 2019']
+
+fig, axes= plt.subplots(1,2, figsize=(3,6))
+plt.style.use('ggplot')
+explode = (0.03, 0.03, 0.03, 0.03, 0.03,0.03, 0.03)
+
+for ax, col in zip(axes, df3.columns):
+    ax.pie(df3[col],labels=df3.index, radius=.85,explode=explode,autopct='%1.1f%%', wedgeprops=dict(width=.22))
+    ax.set_title(col, fontsize=14)
 plt.show()
